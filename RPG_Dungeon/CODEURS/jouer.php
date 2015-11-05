53d3285321bf017fbf82b0cfff38e7e1e3b57a59
WACKERD_INFO <WACKERD_INFO@CFPI-R105PC03>
2015-11-05 15:18:06 +0100

    -Bouton déplacement ajouté -Lien vers la page jouer ajouté -Page jouer ajouté -Image flèches ajouté

---
+++ RPG_Dungeon/CODEURS/jouer.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+
+        <meta charset="utf-8">
+        <title>Maquette</title>
+        <link href="./script/css/bootstrap.min.css" rel="stylesheet">
+        <link href="./script/css/style.css" rel="stylesheet">
+
+    </head>
+    <body>
+        <header class="container page-header">
+            <img class="img-responsive col-sm-10 col-sm-offset-1" src="./IMG/logo dungeon.png">
+        </header>
+        <section>
+            <div class="container contenu">
+
+                <div class="modal-header col-sm-6 col-sm-offset-3">
+                    <h1 class="text-center">Maquette</h1>
+                </div>
+                <div class="modal-body col-sm-6 col-sm-offset-3">
+                    <a  href=""><img src="./IMG/NORDOUEST.jpg"></a>
+                    <a  href=""><img src="./IMG/NORD.jpg"></a>
+                    <a  href=""><img src="./IMG/NORDEST.jpg"></br></a>
+                    <a  href=""><img src="./IMG/OUEST.jpg"></a>
+                    <a  href=""><img src="./IMG/SUD.jpg"></a>
+                    <a  href=""><img src="./IMG/EST.jpg"></a>
+
+                </div>
+            </div>
+        </section>
+
+        <footer class="container">
+            <p class="navbar-text">
+                Technicien Informatique ES 2015 T.IS-E1AB 
+            </p>
+        </footer>
+    </body>
+</html>
+
+
