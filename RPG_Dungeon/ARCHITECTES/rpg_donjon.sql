4fddc5caf903d572e7183729e2255bb9ce5a8f2d
Devaud <devaud.alan@gmail.com>
2015-11-19 13:17:49 +0100

    Changement de la base de données
    	- Ajout d'une table associative entre Reference et texture
    	- Ajout de la table Utilisateurs

--- RPG_Dungeon/ARCHITECTES/rpg_donjon.sql
+++ RPG_Dungeon/ARCHITECTES/rpg_donjon.sql
@@ -1,13 +1,9 @@
--- BDD User : UserDonjon
--- BDD PassWord : Super
--- BDD Name : RPG_DONJON
--- 
 -- phpMyAdmin SQL Dump
 -- version 4.1.4
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Jeu 05 Novembre 2015 à 13:18
+-- Généré le :  Jeu 19 Novembre 2015 à 13:14
 -- Version du serveur :  5.6.15-log
 -- Version de PHP :  5.4.24
 
@@ -23,13 +19,31 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `rpg_donjon`
 --
-CREATE DATABASE IF NOT EXISTS "RPG_DONJON"
+CREATE DATABASE IF NOT EXISTS `rpg_donjon` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `rpg_donjon`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `associertexture`
+--
+
+DROP TABLE IF EXISTS `associertexture`;
+CREATE TABLE IF NOT EXISTS `associertexture` (
+  `idReference` int(11) NOT NULL,
+  `idTexture` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  PRIMARY KEY (`idReference`,`idTexture`),
+  KEY `idTexture` (`idTexture`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `plateformes`
 --
 
+DROP TABLE IF EXISTS `plateformes`;
 CREATE TABLE IF NOT EXISTS `plateformes` (
   `x` int(11) NOT NULL,
   `y` int(11) NOT NULL,
@@ -42,43 +56,44 @@ CREATE TABLE IF NOT EXISTS `plateformes` (
 --
 -- Contenu de la table `plateformes`
 --
+
 INSERT INTO `plateformes` (`x`, `y`, `Direction`, `idReference`) VALUES
-('0', '0', NULL, '2'), 
-('0', '1', NULL, '2'),
-('0', '2', NULL, '2'),
-('0', '3', NULL, '2'), 
-('0', '4', NULL, '2'), 
-('0', '5', NULL, '2'), 
-('1', '0', NULL, '2'), 
-('1', '1', NULL, '1'), 
-('1', '2', NULL, '1'), 
-('1', '3', NULL, '1'), 
-('1', '4', NULL, '2'), 
-('1', '5', NULL, '2'), 
-('2', '0', NULL, '2'), 
-('2', '1', NULL, '2'), 
-('2', '2', NULL, '2'), 
-('2', '3', NULL, '1'), 
-('2', '4', NULL, '2'),
-('2', '5', NULL, '2'), 
-('3', '0', NULL, '2'), 
-('3', '1', NULL, '1'),
-('3', '2', NULL, '2'), 
-('3', '3', NULL, '3'),
-('3', '4', NULL, '1'), 
-('3', '5', NULL, '2'),
-('4', '0', NULL, '2'),
-('4', '1', NULL, '1'),
-('4', '2', NULL, '1'),
-('4', '3', NULL, '1'),
-('4', '4', NULL, '1'),
-('4', '5', NULL, '2'),
-('5', '0', NULL, '2'), 
-('5', '1', NULL, '2'),
-('5', '2', NULL, '2'),
-('5', '3', NULL, '2'), 
-('5', '4', NULL, '2'), 
-('5', '5', NULL, '2');
+(0, 0, '', 2),
+(0, 1, '', 2),
+(0, 2, '', 2),
+(0, 3, '', 2),
+(0, 4, '', 2),
+(0, 5, '', 2),
+(1, 0, '', 2),
+(1, 1, '', 1),
+(1, 2, '', 1),
+(1, 3, '', 1),
+(1, 4, '', 2),
+(1, 5, '', 2),
+(2, 0, '', 2),
+(2, 1, '', 2),
+(2, 2, '', 2),
+(2, 3, '', 1),
+(2, 4, '', 2),
+(2, 5, '', 2),
+(3, 0, '', 2),
+(3, 1, '', 1),
+(3, 2, '', 2),
+(3, 3, '', 3),
+(3, 4, '', 1),
+(3, 5, '', 2),
+(4, 0, '', 2),
+(4, 1, '', 1),
+(4, 2, '', 1),
+(4, 3, '', 1),
+(4, 4, '', 1),
+(4, 5, '', 2),
+(5, 0, '', 2),
+(5, 1, '', 2),
+(5, 2, '', 2),
+(5, 3, '', 2),
+(5, 4, '', 2),
+(5, 5, '', 2);
 
 -- --------------------------------------------------------
 
@@ -86,22 +101,21 @@ INSERT INTO `plateformes` (`x`, `y`, `Direction`, `idReference`) VALUES
 -- Structure de la table `references`
 --
 
+DROP TABLE IF EXISTS `references`;
 CREATE TABLE IF NOT EXISTS `references` (
   `idReference` int(11) NOT NULL AUTO_INCREMENT,
   `Valeur` varchar(15) NOT NULL,
-  `idTexture` int(11) NOT NULL,
-  PRIMARY KEY (`idReference`),
-  KEY `idTexture` (`idTexture`)
+  PRIMARY KEY (`idReference`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
 --
 -- Contenu de la table `references`
 --
 
-INSERT INTO `references` (`idReference`, `Valeur`, `idTexture`) VALUES
-(1, 'sol', 0),
-(2, 'mur', 0),
-(3, 'joueur', 0);
+INSERT INTO `references` (`idReference`, `Valeur`) VALUES
+(1, 'sol'),
+(2, 'mur'),
+(3, 'joueur');
 
 -- --------------------------------------------------------
 
@@ -109,12 +123,54 @@ INSERT INTO `references` (`idReference`, `Valeur`, `idTexture`) VALUES
 -- Structure de la table `textures`
 --
 
+DROP TABLE IF EXISTS `textures`;
 CREATE TABLE IF NOT EXISTS `textures` (
   `idTexture` int(11) NOT NULL AUTO_INCREMENT,
   `Nom` varchar(100) NOT NULL,
   `Chemin` varchar(200) NOT NULL,
   PRIMARY KEY (`idTexture`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Contenu de la table `textures`
+--
+
+INSERT INTO `textures` (`idTexture`, `Nom`, `Chemin`) VALUES
+(1, 'sol1', 'sol1.png'),
+(2, 'sol2', 'sol2.png');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateurs`
+--
+
+DROP TABLE IF EXISTS `utilisateurs`;
+CREATE TABLE IF NOT EXISTS `utilisateurs` (
+  `idUtilisateur` int(11) NOT NULL AUTO_INCREMENT,
+  `pseudo` varchar(25) NOT NULL,
+  `mdp` varchar(50) NOT NULL,
+  `posX` int(10) NOT NULL,
+  `posY` int(10) NOT NULL,
+  PRIMARY KEY (`idUtilisateur`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `associertexture`
+--
+ALTER TABLE `associertexture`
+  ADD CONSTRAINT `associertexture_ibfk_2` FOREIGN KEY (`idReference`) REFERENCES `references` (`idReference`),
+  ADD CONSTRAINT `associertexture_ibfk_1` FOREIGN KEY (`idTexture`) REFERENCES `textures` (`idTexture`);
+
+--
+-- Contraintes pour la table `plateformes`
+--
+ALTER TABLE `plateformes`
+  ADD CONSTRAINT `plateformes_ibfk_1` FOREIGN KEY (`idReference`) REFERENCES `references` (`idReference`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
