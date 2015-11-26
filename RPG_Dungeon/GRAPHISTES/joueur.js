ddc72507cdc4a97e8486897387c0932e4d605698
admintech <admintech@CFPI-R105PC09>
2015-11-26 13:09:27 +0100

    Creations de divers fichiers 	+ joueur.js - ajout de la distance de la vue (propriété) 	+ joueur.js - initialisation du tableau de la vue du joueur à 99 (99 -> vide)  	+ TEMP.html - correction d'un bug pour trouver la position du joueur  	+ TEMP.html - commencement de la fonction de création de la vue en fonction de la direction (InitialisationTableauValue)[Manque EST/OUEST]

--- RPG_Dungeon/GRAPHISTES/joueur.js
+++ RPG_Dungeon/GRAPHISTES/joueur.js
@@ -6,9 +6,65 @@
  *               dessiner la vue, via les informations de la classe niveau.
  */
 
-var direction = "NORD"; //valeurs possibles : NORD-SUD-EST-OUEST
 
-function joueur() 
+//Constructeur de la classe joueur
+function joueur()
 {
+    this.direction = "NORD"; //valeurs possibles : NORD-SUD-EST-OUEST
+    //direction = direction donnée par les architèctes
     
+    this.distanceVue = 4;
+    
+    this.joueurX;
+    this.joueurY;
+    
+    this.vueJoueur = [
+        [1,1,1,1,1],
+        [1,1,1,1,1],
+        [1,1,1,1,1],
+        [1,1,1,1,1],
+        [1,NULL,1,1,1]
+    ];
+
+    //formule pour calculer les plans
+
+
+    //créer la vue du joueur
+    this.creationDeLaVue = function(canvas, ctx) {
+        this.centerX = canvas.width / 2;
+        this.centerY = canvas.height / 2.5;
+
+        ctx.beginPath();
+        ctx.moveTo(this.centerX, this.centerY);
+        ctx.lineTo(0, 0);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(this.centerX, this.centerY);
+        ctx.lineTo(canvas.width, 0);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(this.centerX, this.centerY);
+        ctx.lineTo(canvas.width, canvas.height);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(this.centerX, this.centerY);
+        ctx.lineTo(0, canvas.height);
+        ctx.stroke();
+
+        for (var i = 150; i < 800; i += 200) {
+            this.tailleTile = i;
+            this.posX = this.tailleTile / 2;
+            this.posY = (this.posX / 5) * 4;
+
+            ctx.rect(this.centerX - this.posX, this.centerY - this.posY, this.tailleTile, this.tailleTile);
+            if (i < 160){
+                ctx.fill();
+            } else {
+                ctx.stroke();
+            }
+        }
+    };
 }
--- RPG_Dungeon/GRAPHISTES/niveau.js
+++ RPG_Dungeon/GRAPHISTES/niveau.js
@@ -6,6 +6,7 @@
  *               au niveau.
  */
 
+
     var carte = [
         [1,1,1,1,1,1],
         [1,1,0,0,0,0],
@@ -13,7 +14,22 @@
         [1,1,0,1,1,1],
         [1,1,1,1,1,1]
     ];    
-    var faces = [
+    var faces = [];
+
+//constructeur de la classe niveau
+function niveau()
+{
+    //carte = tableau donné par les architectes
+    this.carte = [
+        [1,1,1,1,1,1],
+        [1,0,0,0,1,1],
+        [1,0,2,1,1,1],
+        [1,1,0,1,0,1],
+        [1,1,1,1,1,1]
+    ];
+    
+    this.faces = [
+
         "Sol1.png",
         "Sol2.png",
         "Sol3.png",
@@ -39,10 +55,54 @@
         "MurCoteG3.png",
         "MurCoteG4.png"
     ];
-
+    
+}
+/* 
+ * AUTEUR      : Haury, De Biasi, Salvi, Bartiban, Panniz
+ * DATE        : 5.11.2015
+ * VERSION     : 1.0
+ * DESCIRPTION : Classe niveau - permet de stocker toutes les informations relatives
+ *               au niveau.
+ */
 
 //constructeur de la classe niveau
 function niveau()
 {
     //carte = tableau donné par les architectes
+    this.carte = [
+        [1,1,1,1,1,1],
+        [1,0,0,0,1,1],
+        [1,0,2,1,1,1],
+        [1,1,0,1,0,1],
+        [1,1,1,1,1,1]
+    ];
+    
+    this.faces = [
+        "Sol1.png",
+        "Sol2.png",
+        "Sol3.png",
+        "Sol4.png",
+        
+        "Plafond1.png",
+        "Plafond2.png",
+        "Plafond3.png",
+        "Plafond4.png",
+        
+        "MurFace1.png",
+        "MurFace2.png",
+        "MurFace3.png",
+        "MurFace4.png",
+        
+        "MurCoteD1.png",
+        "MurCoteD2.png",
+        "MurCoteD3.png",
+        "MurCoteD4.png",
+        
+        "MurCoteG1.png",
+        "MurCoteG2.png",
+        "MurCoteG3.png",
+        "MurCoteG4.png"
+    ];
+    
 }
+// Creations de divers fichiers 	+ joueur.js - ajout de la distance de la vue (propriété) 	+ joueur.js - initialisation du tableau de la vue du joueur à 99 (99 -> vide) 	+ TEMP.html - correction d'un bug pour trouver la position du joueur 	+ TEMP.html - commencement de la fonction de création de la vue en fonction de la direction (InitialisationTableauValue)[Manque EST/OUEST]
